b94f734ff2d0d836501986c95f1d175056f7d0b9
Chun-gu <chungu.yi92@gmail.com>
2023-03-09 21:56:05 +0900

    :sparkles: 상품 카드에 스타일과 모달 적용

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,34 +1,89 @@
-import useProduct from '../hooks/useProduct';
+import { useCallback } from 'react';
+
+import {
+  Badge,
+  Box,
+  Button,
+  Image,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+
+import { useProduct } from '../hooks';
+import DetailModal from './DetailModal';
 
 export default function ProductCard({ product }) {
   const { idx, name, mainImage, price, spaceCategory, isReserved } = product;
 
-  const { makeReservation, cancelReservation } = useProduct();
+  const [, { makeReservation, cancelReservation }] = useProduct();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  function onClickReserve() {
-    makeReservation(idx);
-  }
+  const onClickReserve = useCallback(
+    (e) => {
+      e.stopPropagation();
+      makeReservation(idx);
+    },
+    [idx, makeReservation],
+  );
 
-  function onClickCancel() {
-    cancelReservation(idx);
-  }
+  const onClickCancel = useCallback(
+    (e) => {
+      e.stopPropagation();
+      cancelReservation(idx);
+    },
+    [idx, cancelReservation],
+  );
 
   return (
-    <div>
-      <img src={mainImage} alt="여행 상품 대표 이미지" />
-      <span>{idx}</span>
-      <span>{name}</span>
-      <span>{price}</span>
-      <span>{spaceCategory}</span>
-      {isReserved ? (
-        <button type="button" onClick={onClickCancel}>
-          취소
-        </button>
-      ) : (
-        <button type="button" onClick={onClickReserve}>
-          예약
-        </button>
+    <>
+      <Box
+        width="410px"
+        height="96px"
+        display="flex"
+        flexGrow="1"
+        borderRadius="10px"
+        overflow="hidden"
+        boxShadow="base"
+        onClick={onOpen}
+        tabIndex="0"
+        role="button">
+        <Image
+          role="button"
+          boxSize="100px"
+          objectFit="cover"
+          src={mainImage}
+          alt="여행 상품 대표 이미지"
+        />
+        <Box
+          p="8px"
+          display="flex"
+          flexDir="column"
+          flexGrow="1"
+          justifyContent="space-between">
+          <Box display="flex" justifyContent="space-between">
+            <Text fontSize="md">{`#${idx}`}</Text>
+            <Badge colorScheme="blue">{`@${spaceCategory}`}</Badge>
+          </Box>
+          <Text size="md" fontWeight="bold">
+            {name}
+          </Text>
+          <Box display="flex" justifyContent="space-between">
+            <Text size="md">{price.toLocaleString()}원</Text>
+            {isReserved ? (
+              <Button size="xs" colorScheme="red" onClick={onClickCancel}>
+                취소
+              </Button>
+            ) : (
+              <Button size="xs" colorScheme="green" onClick={onClickReserve}>
+                예약
+              </Button>
+            )}
+          </Box>
+        </Box>
+      </Box>
+      {isOpen && (
+        <DetailModal isOpen={isOpen} onClose={onClose} product={product} />
       )}
-    </div>
+    </>
   );
 }
